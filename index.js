c1419b943a51d85cd507a224753c8c79baf8de1d
Igor Depolli <igorcler@gmail.com>
2021-06-14 14:39:15 -0300

    Preparing project

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+const sequelize = require('./src/database');
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Authorization, Content-Type, Accept');
+  app.use(cors());
+  next();
+});
+
+app.use(express.json());
+app.use(routes);
+
+sequelize.sync({ force: true }).then(() => {
+  app.listen(3000, () => {
+    // eslint-disable-next-line no-console
+    console.log('Server running in http://localhost:3000');
+  });
+});
---
+++ routes.js
@@ -0,0 +1,6 @@
+// eslint-disable-next-line no-unused-vars
+const express = require('express');
+
+const routes = express.Router();
+
+module.exports = routes;
