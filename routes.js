4c69f3c843d105f3c1f5c4bfc5eb4501b7a36193
Igor Depolli <igorcler@gmail.com>
2021-06-15 19:23:47 -0300

    ProfileController get method started

--- routes.js
+++ routes.js
@@ -3,10 +3,13 @@ const express = require('express');
 
 const UserController = require('./src/controllers/UserController');
 const SessionController = require('./src/controllers/SessionController');
+const ProfileController = require('./src/controllers/ProfileController');
 
 const routes = express.Router();
 
 routes.post('/api/users/login', SessionController.login);
 routes.post('/api/users', UserController.store);
+
+routes.get('/api/profiles/:username', ProfileController.get);
 
 module.exports = routes;
--- src/controllers/ProfileController.js
+++ src/controllers/ProfileController.js
@@ -5,22 +5,31 @@ class ProfileController {
     try {
       const { username } = req.params;
 
-      const profile = await User.findOne({ where: { username } });
+      const profile = await User.findOne({ where: { username }, include: ['Followers'] });
 
       if (!profile) {
         throw new Error(`Profile ${username} not found!`);
       }
 
-      // let following = false;
+      let following = false;
 
-      const user = await User.findByPk(req.user.id);
+      if (req.body.user) {
+        const user = await User.findOne({ where: { username } });
+
+        for (let i = 0; i < profile.Followers.length; i += 1) {
+          if (profile.Followers[i] === user.id) {
+            following = true;
+          }
+        }
+      }
 
       const { bio, image } = profile;
       return res.json({
         profile: {
           username,
           bio,
-          image
+          image,
+          following
         }
       });
     } catch (error) {
